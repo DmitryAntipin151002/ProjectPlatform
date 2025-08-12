e04f074b3afbbc7f29f0cdec6c10165c6c2df99d
DmitryAntipin <dmitry1986antipin@gmail.com>
2025-08-12 11:55:08 +0300

    a

---
+++ Code/Back/GameHubBack/src/test/java/com/example/gamehubback/GameHubBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamehubback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameHubBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
